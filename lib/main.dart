562a56f4bc3222bb1caa5a3893edfe9df4c38747
Adhiraj772 <adhirajsaxena123@gmail.com>
2021-11-25 01:51:20 +0530

    App is displaying questions and basically checking the answers. The only thing is that it crashes when it crosses index 2. There is only a single class.

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,15 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+
+  List <Icon> scoreKeeper = [];
+  List<String> questions =[
+    'You can lead a cow down stairs  but not upstairs.',
+    'Approximately one quarter of human bones are in the feet',
+    'A slug blood is green.'
+  ];
+  List<bool> answers = [false,true,true];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +71,19 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[questionNumber];
+                if(correctAnswer==true){
+                  print('user got it right');
+                }
+                else{
+                  print('user got it wrong');
+                }
+
+                setState(() {
+                  questionNumber++;
+                }
+                );
+
               },
             ),
           ),
@@ -80,11 +102,24 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = answers[questionNumber];
+                if(correctAnswer==false){
+                  print('user got it right');
+                }
+                else{
+                  print('user got it wrong');
+                }
+                setState(() {
+                  questionNumber++;
+                });
+
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children:scoreKeeper,
+        )
       ],
     );
   }
