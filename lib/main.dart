8fdcba446f7cc5472b08dcc0359d245f1565a303
Adhiraj772 <adhirajsaxena123@gmail.com>
2021-11-28 14:49:21 +0530

    Ok so now we have created two new class. One is the question class which is gonna have two properties and that are the question and its corrrect answer, so now we have a single class which is gonna display the question as well as gonna handle the correctness of user. The other class is questionBank which is gonna contain our questions. Suppose if I wanna change the quiz to say The Office Quiz, then I just need to make changes in that quizBrain class,and then I am good to go.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = new QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -34,11 +36,7 @@ class _QuizPageState extends State<QuizPage> {
   //   'A slug blood is green.'
   // ];
   // List<bool> answers = [false,true,true];
-  List<Question> questionBank = [
-    Question(q:'You can lead a cow down stairs  but not upstairs.',a: false ),
-    Question(q:'Approximately one quarter of human bones are in the feet',a: true ),
-    Question(q:'A slug blood is green.',a: true ),
-  ];
+
   int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -52,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -77,7 +75,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                 if(correctAnswer==true){
                   print('user got it right');
                 }
@@ -108,7 +106,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer = questionBank[questionNumber].questionAnswer;
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                 if(correctAnswer==false){
                   print('user got it right');
                 }
