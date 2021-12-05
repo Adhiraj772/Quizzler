a71760ef9415c6eba38bc6a67e6b23a77e8cf59c
Adhiraj772 <adhirajsaxena123@gmail.com>
2021-12-05 20:33:17 +0530

    OK so now what we have done is that now we have added scoreKeeper finally. So in the checkAnswer function we made changes such that on correct answer it adds check icon on correct answer and on wrong answer it adds cross icon.

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,20 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
 
    List <Icon> scoreKeeper = [];
+   void checkAnswer(bool userPicked){
+     bool correctAnswer = quizBrain.getCorrectAnswer();
+     setState(() {
+     if(userPicked==correctAnswer){
+       scoreKeeper.add(Icon(Icons.check,color: Colors.green,));
+     }
+     else{
+       scoreKeeper.add(Icon(Icons.close,color: Colors.red,));
+     }
+
+
+       quizBrain.nextQuestion();
+     });
+   }
   // List<String> questions =[
   //   'You can lead a cow down stairs  but not upstairs.',
   //   'Approximately one quarter of human bones are in the feet',
@@ -75,18 +89,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer = quizBrain.getCorrectAnswer();
-                if(correctAnswer==true){
-                  print('user got it right');
-                }
-                else{
-                  print('user got it wrong');
-                }
-
-                setState(() {
-                  quizBrain.nextQuestion();
-                }
-                );
+                checkAnswer(true);
 
               },
             ),
@@ -106,16 +109,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer = quizBrain.getCorrectAnswer();
-                if(correctAnswer==false){
-                  print('user got it right');
-                }
-                else{
-                  print('user got it wrong');
-                }
-                setState(() {
-                  quizBrain.nextQuestion();
-                });
+                checkAnswer(false);
 
               },
             ),
